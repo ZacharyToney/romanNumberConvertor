a02297da23e0a2dbe71096421d363aed713c557b
ZacharyToney <zach@clearseries.com>
2018-09-07 19:08:47 -0400

    add roman to arabic functions and check value of input

--- src/romannumberconvertor/RomanNumberConvertor.java
+++ src/romannumberconvertor/RomanNumberConvertor.java
@@ -1,21 +1,77 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package romannumberconvertor;
 
-/**
- *
- * @author Zachary
- */
-public class RomanNumberConvertor {
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
 
-    /**
+public class RomanNumberConvertor {
+    
+    // This function returns value of a Roman symbol
+    public int valueOfRomanNumeral(char x)
+    {
+        if (x == 'I')
+            return 1;
+        if (x == 'V')
+            return 5;
+        if (x == 'X')
+            return 10;
+        if (x == 'L')
+            return 50;
+        if (x == 'C')
+            return 100;
+        if (x == 'D')
+            return 500;
+        if (x == 'M')
+            return 1000;
+        return -1;
+    }
+    
+    // Finds the value of the roman numeral string
+    public int romanToArabic(String str)
+    {   
+        int result = 0;
+        for (int i=0; i<str.length(); i++)
+        {
+            int char1 = valueOfRomanNumeral(str.charAt(i));
+            if (i+1 <str.length())
+            {
+                int char2 = valueOfRomanNumeral(str.charAt(i+1));
+                if (char1 >= char2)
+                {
+                    result = result + char1;
+                }
+                else
+                {
+                    result = result + char2 - char1;
+                    i++; 
+                }
+            }
+            else
+            {
+                result = result + char1;
+                i++;
+            }
+        }
+        return result;
+    }
+    
+     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        System.out.println("Please enter a roman numeral: ");
+        
+        BufferedReader reader =  
+                   new BufferedReader(new InputStreamReader(System.in)); 
+          
+        String str = reader.readLine(); 
+  
+        System.out.println(str);
+        
+        RomanNumberConvertor r2a = new RomanNumberConvertor();
+ 
+        System.out.println("Integer form of Roman Numeral" +
+                           " is " + r2a.romanToArabic(str));
     }
     
 }
