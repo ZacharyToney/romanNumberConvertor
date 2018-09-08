e396654b404d6410caa299d1d654c7a193a1cdb4
evanrjfischer <evanrjfischer@gmail.com>
2018-09-07 22:57:49 -0400

    Add arabicToRoman Conversion Functionality
    Made the buildTreeMap() and arabicToRoman() functions which reference a TreeMap to turn arabic numbers into roman numerals.

--- src/romannumberconvertor/RomanNumberConvertor.java
+++ src/romannumberconvertor/RomanNumberConvertor.java
@@ -1,21 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package romannumberconvertor;
 
-/**
- *
- * @author Zachary
- */
-public class RomanNumberConvertor {
+import java.util.TreeMap;
 
-    /**
-     * @param args the command line arguments
-     */
+public class RomanNumberConvertor {
+	static TreeMap<Integer, String> treeMap = new TreeMap<Integer, String>();
+
     public static void main(String[] args) {
-        // TODO code application logic here
+       buildTreeMap();
     }
     
+    private static void buildTreeMap() {
+        treeMap.put(1, "I");
+        treeMap.put(4, "IV");
+        treeMap.put(5, "V");
+        treeMap.put(9, "IX");
+        treeMap.put(10, "X");
+        treeMap.put(40, "XL");
+        treeMap.put(50, "L");
+        treeMap.put(90, "XC");
+        treeMap.put(100, "C");
+        treeMap.put(400, "CD");
+        treeMap.put(500, "D");
+        treeMap.put(900, "CM");
+        treeMap.put(1000, "M");
+    }
+    
+    private static String arabicToRoman(int arabicNumber) {
+    	  int closestKey = treeMap.floorKey(arabicNumber);
+    	  if(arabicNumber != closestKey) {
+    		  return treeMap.get(closestKey) + arabicToRoman(arabicNumber - closestKey);
+    	  }else{
+    		  return treeMap.get(arabicNumber);
+    	  }
+    }
 }
--- test/mainJunitTest.java
+++ test/mainJunitTest.java
@@ -4,41 +4,8 @@
  * and open the template in the editor.
  */
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-/**
- *
- * @author Zachary
- */
 public class mainJunitTest {
     
     public mainJunitTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 }
