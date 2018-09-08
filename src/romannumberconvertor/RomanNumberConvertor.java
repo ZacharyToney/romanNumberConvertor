357649a540917ce31bb68a7b785049c9c205bfa3
evanrjfischer <evanrjfischer@gmail.com>
2018-09-08 01:33:22 -0400

    Added Test for Arabic to Roman Conversion
    I added tests for the aravicToRoman() function. I made tests for the minimum and maximum possible numbers that can be converted into roman numerals. Made sure the function can function when the given integer is not inside of the tree map. Also made sure it can handle integers with one, two, three, or four digits.

--- src/romannumberconvertor/RomanNumberConvertor.java
+++ src/romannumberconvertor/RomanNumberConvertor.java
@@ -6,10 +6,18 @@ public class RomanNumberConvertor {
 	static TreeMap<Integer, String> treeMap = new TreeMap<Integer, String>();
 
     public static void main(String[] args) {
-       buildTreeMap();
+
     }
     
-    private static void buildTreeMap() {
+    /*
+     The buildTreeMap() function populates treeMap.
+     Each key is an integer with a String value.
+     This function includes all letters necessary
+     for converting numbers between 1 and 4,999 to
+     roman numerals.
+     */
+    
+    static void buildTreeMap() {
         treeMap.put(1, "I");
         treeMap.put(4, "IV");
         treeMap.put(5, "V");
@@ -25,7 +33,19 @@ public class RomanNumberConvertor {
         treeMap.put(1000, "M");
     }
     
-    private static String arabicToRoman(int arabicNumber) {
+    /*
+    The arabicToRoman() function does all of the magic.
+    It is a recursive function. Using a tree map allows 
+    us to use the .floorkey() function to find the first 
+    possible key smaller than the given arabicNumber. It then
+    calls the function again using the difference between the
+    original arabicNumber and its closestKey. However, if the given 
+    arabicNumber matches on of the keys exactly, it will return 
+    the value paired with it immediately. 
+    */
+    
+    public static String arabicToRoman(int arabicNumber) {
+    	  buildTreeMap();
     	  int closestKey = treeMap.floorKey(arabicNumber);
     	  if(arabicNumber != closestKey) {
     		  return treeMap.get(closestKey) + arabicToRoman(arabicNumber - closestKey);
